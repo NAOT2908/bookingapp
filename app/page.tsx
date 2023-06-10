0b74db9b9b235c2ecf5b90943506741d70728be3
NAOT2908 <ntoan29082000@gmail.com>
2023-06-10 17:37:59 +0700

    updatenew

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import EmptyState from './components/EmptyState';
 import getListings, { IListingsParams } from './actions/getListings';
 import ListingCard from './components/listings/ListingCard';
 import getCurrentUser from './actions/getCurrentUser';
+export const dynamic = 'force-dynamic'
 
 interface HomeProps{
   searchParams: IListingsParams
